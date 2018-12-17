a4387ff73f8c4e50524f1f94bd9e3b44c98fcb32
Fredrik Holmdahl <holmdahl.fredrik@gmail.com>
2018-12-17 17:24:03 +0100

    database working, Role object not done

--- PrepLight/Role.swift
+++ PrepLight/Role.swift
@@ -10,7 +10,35 @@ import Foundation
 
 
 struct Role {
-    var title: String
-    var description: String
-    var company: String
+    var title: String?
+    var description: String?
+    var company: String?
+    var questions: NSDictionary?
+    var reminders: NSDictionary?
+    var checklist: NSDictionary?
+    
+    init(assignment: NSDictionary) {
+        if let roleTitle = assignment.value(forKey: "title") as? String{
+            self.title = roleTitle
+        }
+        
+        if let roleDescription = assignment.value(forKey: "description") as? String{
+            self.description = roleDescription
+        }
+        
+        if let roleCompany = assignment.value(forKey: "company") as? String{
+            self.company = roleCompany
+        }
+        
+        if let roleQuestions = assignment.value(forKey: "questions") as? NSDictionary{
+            self.questions = roleQuestions
+        }
+        
+        if let roleReminders = assignment.value(forKey: "reminders") as? NSDictionary{
+            self.reminders = roleReminders
+        }
+        if let roleChecklist = assignment.value(forKey: "checklist") as? NSDictionary {
+            self.checklist = roleChecklist
+        }
+    }
 }
